9dca1bf660788ea3685d208d9997668b49e75bc9
Mansi Bhegade <mansibhegade123@gmail.com>
2025-02-28 12:10:30 +0530

    new again

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://your-backend.onrender.com/graphql", // Replace with your actual backend URL
   cache: new InMemoryCache(),
 });
+
 
 export { client, ApolloProvider };
